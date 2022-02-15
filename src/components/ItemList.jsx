821dc0ec8a6609caa8b5650ca16390ef2c33c1e6
zcaldwell <mintyst.john@gmail.com>
2022-02-15 15:35:16 -0800

    adding working, edit next

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-export default function ItemList() {
-  return <div>ItemList</div>;
+export default function ItemList({ items, handleDeleteItem }) {
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h1>{item.text}</h1>
+            <button type="button" onClick={() => handleDeleteItem(item.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/views/ShoppingList/ShoppingList.jsx
+++ src/views/ShoppingList/ShoppingList.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useReducer, useState } from 'react';
 import ItemList from '../../components/ItemList';
 
-const initialItems = [];
+const initialItems = [
+  { id: 0, text: 'Meat 🍖', done: false },
+  { id: 1, text: 'Potatoes 🥔', done: false },
+  { id: 2, text: 'Broccoli 🥦', done: false },
+];
 
 function itemReducer(items, action) {
   switch (action.type) {
     case 'add': {
-      return [...items, { id: action.id, item: action.text }];
+      return [...items, { id: action.id, text: action.text }];
     }
     case 'delete': {
       return items.filter((item) => item.id !== action.id);
@@ -18,6 +22,42 @@ function itemReducer(items, action) {
 }
 
 export default function ShoppingList() {
-    const [items, dispatch] = 
-  return <div></div>;
+  const [items, dispatch] = useReducer(itemReducer, initialItems);
+  const [newItem, setNewItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNewItem('');
+    handleAddItem(newItem);
+  };
+
+  const handleAddItem = (text) => {
+    dispatch({ type: 'add', id: items.length + 1, text });
+  };
+
+  const handleDeleteItem = (taskId) => {
+    dispatch({ type: 'delete', id: taskId });
+  };
+
+  const handleEditItem = (task) => {
+    dispatch({ type: 'edit', task });
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Enter item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+          <button type="submit">Add Item</button>
+        </form>
+      </div>
+      <div>
+        <ItemList items={items} handleDeleteItem={handleDeleteItem} />
+      </div>
+    </>
+  );
 }
